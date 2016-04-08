3b2d8dd0d5a36e67898bba1e10ffa7ae9ff04e6f
xuanqisong <xuanlll19900808@sina.com>
2016-04-08 12:13:29 +0800

    new monitor script it is change a new model can write ini file and create table

--- relevance_programme/routerip_used/routerbakfile.py
+++ relevance_programme/routerip_used/routerbakfile.py
@@ -3,58 +3,41 @@
 import time
 import paramiko
 import os
-
-
-# 写文件函数
-def write_file(filepath, txt, method):
-    try:
-        f = open(filepath, method)
-        txt_type = str(type(txt))[7:12]
-        if txt_type == 'tuple':
-            for va in txt:
-                f.write(str(va))
-        elif txt_type == 'list':
-            for va in txt:
-                f.write(str(va))
-        elif txt_type == 'dict':
-            for va in txt.iteritems():
-                f.write(str(va[0]))
-                f.write(':')
-                f.write(str(va[1]))
-        else:
-            f.write(str(txt))
-        f.write('\n')
-        f.close
-    except Exception as e:
-        print "write file error :", txt
-        print "file path :", filepath
-        print "error message :", e
-        exit()
-
+from Tools.global_function import write_file, decrypt, tuple_to_list
+from Tools.DBtools import MysqlDb
+from Tools import global_value
+from Tools.global_class import Server
 
 class ParamikoRun(object):
-    def __init__(self, ip, port, user, password):
-        self.ip = ip
-        self.port = port
-        self.user = user
-        self.password = password
+    def __init__(self, server):
+        self.server = server
 
     def getsshconnect(self):
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(self.ip, int(self.port), self.user, self.password)
+        ssh.connect(self.server.ip, int(self.server.port), self.server.user,
+                    self.server.password)
         return ssh
 
 
 if __name__ == "__main__":
-    user = 'xuanqisong'
-    password = 'xuanqisong@123'
-    ip_father_list = ['10.10.15.126', '10.10.15.124', '10.10.15.125']
+    mysql = MysqlDb('mysql-host')
+    sql = "SELECT * FROM server_list_old WHERE ip='10.255.254.131'"
+    rs_tuple = mysql.run_sql(sql)
+    rs_list = tuple_to_list(rs_tuple)
+    server_list = []
+    for li in rs_list:
+        server = Server(li)
+        server.binding_server_value()
+        server_list.append(server)
+    # user = 'xuanqisong'
+    # password = 'xuanqisong@123'
+    # ip_father_list = ['10.10.15.126', '10.10.15.124', '10.10.15.125']
     # ip_father_list = ['10.10.15.126']
     paramikodi = {}
     ip_file = {}
-    for ip in ip_father_list:
-        paramikodi[ip] = ParamikoRun(ip, 22, user, password)
+    for server in server_list:
+        paramikodi[server.ip] = ParamikoRun(server)
 
     for ip, paramikorun in paramikodi.items():
         ssh = paramikorun.getsshconnect()
@@ -69,11 +52,11 @@ if __name__ == "__main__":
             channel.send(''' ''')
             time.sleep(1)
             re_buff += channel.recv(9999)
-        # 清理无用字符
-        if ip == '10.10.15.126':
-            re_buff = re_buff.replace('  ---- More ----\x1b[42D                                          \x1b[42D', '')
-        else:
-            re_buff = re_buff.replace('  ---- More ----\x1b[16D                \x1b[16D', '')
+        # # 清理无用字符
+        # if ip == '10.10.15.126':
+        re_buff = re_buff.replace('  ---- More ----\x1b[42D                                          \x1b[42D', '')
+        # else:
+        #     re_buff = re_buff.replace('  ---- More ----\x1b[16D                \x1b[16D', '')
 
         ip_file[ip] = re_buff
 
@@ -88,20 +71,20 @@ if __name__ == "__main__":
     # 获取文件上层目录
     dir_name_upup = os.path.abspath(os.path.join(dir_name_up,os.path.pardir))
 
-    dir_name = dir_name_upup+'/servermanager/routerbakfile'
+    dir_name = dir_name_upup + '/servermanager/routerbakfile'
 
     if not os.path.exists(dir_name):
         os.mkdir(dir_name)
 
-    dir_name = dir_name+ '/' + time_now
+    dir_name = dir_name + '/' + time_now
     os.mkdir(dir_name)
     ip_file_name = {}
     for ip in paramikodi:
-        ip_file_name[ip] = dir_name + '/' + ip.replace('.','') + '_configuration.txt'
+        ip_file_name[ip] = dir_name + '/' + ip.replace('.', '') + '_configuration.txt'
 
     # 写文件
     for ip, file_name in ip_file_name.items():
         txt = ip_file[ip]
-        write_file(file_name,txt,'w')
+        write_file(file_name, txt, 'w')
 
 
